af8877b19e8b588900a0760e41cf33bec5770699
Melisa Eugenia Rivas Linares <20245324@esen.edu.sv>
2026-09-19 09:56:20 -0600

    Lab 8

---
+++ trigger-alert-test.js
@@ -0,0 +1,27 @@
+const { CloudWatchClient, PutMetricDataCommand } = require('@aws-sdk/client-cloudwatch');
+
+const REGION = 'us-east-1';
+const cwClient = new CloudWatchClient({ region: REGION });
+
+async function main() {
+  console.log('Enviando metricas de error simuladas para probar la alarma...');
+
+  for (let i = 0; i < 3; i++) {
+    await cwClient.send(new PutMetricDataCommand({
+      Namespace: 'DevSecOpsLab/CheckoutService',
+      MetricData: [{
+        MetricName: 'ErrorCount',
+        Value: 1,
+        Unit: 'Count',
+        Timestamp: new Date(),
+      }],
+    }));
+  }
+
+  console.log('Metricas enviadas. La alarma puede tardar unos minutos en activarse.');
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
